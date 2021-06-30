3bb66dc4be225a9d90ae437bcb0136586b9e39b1
dnanseldev <dnansel.dev@gmail.com>
2021-06-30 16:40:47 -0300

    30/06/2021 16:40

--- dev-info.gd
+++ dev-info.gd
@@ -4,3 +4,5 @@ https://primefaces.org/primeng/showcase/#/
 #NODEJS(DOCKER)
 https://dev.to/oieduardorabelo/10-melhores-praticas-para-aplicacoes-node-js-em-container-com-docker-2b0o
 
+#CHOWN
+vivaolinux.com.br/dica/Entendento-o-comando-chown
