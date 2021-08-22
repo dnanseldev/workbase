1365b82d59eedc579c5b8fe1f6e42ed7190185e6
dnanseldev <dnansel.dev@gmail.com>
2021-08-22 01:34:13 -0300

    ago 22 01:34

---
+++ lnx_crawler/output.gd
@@ -0,0 +1,10 @@
+# echo with -e parameter wich mean it accepts escape caractere(\) ex: \n
+$ echo -e '\nlorenipsun' >> file.txt
+
+# Create an alias through echo command
+# echo -e '\nalias alias_name="command here"' >> ~/.bashrc
+$ echo '\nalias xclip="xclip -sel clip"' >> ~/.bashrc
+
+
+
+
