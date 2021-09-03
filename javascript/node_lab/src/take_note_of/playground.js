05bffd8133d78fa48f3a70d4d8c57822d22e9253
dnanseldev <dnansel.dev@gmail.com>
2021-09-03 16:43:07 -0300

    03-09-2021

--- javascript/node_lab/src/take_note_of/playground.js
+++ javascript/node_lab/src/take_note_of/playground.js
@@ -1,3 +1,38 @@
-const name = 'Daniel Stonebuilt'
+const Util = require('util');
 
-console.log(name)
+function Person()
+{
+    this._firstName = '';
+    this._lastName = ''; 
+    this._joke = 'Jojo Star family';   
+}
+
+Person.prototype.Greet = function() {
+    console.log(`Hello ${this._firstName} ${this._lastName} ${this._joke}`);
+}
+
+function Customer()
+{
+    Person.call(this);
+    this._email = ''
+    this._telephone = ''
+}
+
+Util.inherits(Customer, Person);
+
+//let p1 = new Person();
+
+
+let c1 = new Customer();
+c1._firstName = 'Joseph';
+c1._lastName = 'Biong';
+c1._email = 'joseph@mail.com';
+c1._telephone = '+55(16) 9 8116-6676';
+//c1._joke = 'new joke';
+c1.Greet();
+
+/*
+p1['_firstName'] = 'Daniel';
+p1['_lastName'] = 'Anselmo';
+p1.Greet();
+*/
