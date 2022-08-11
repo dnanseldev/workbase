d7c32a63183a41407cd2fabceb5bb8eb742096ae
dnanseldev <dnansel.dev@gmail.com>
2022-08-11 08:47:25 -0300

    ago 11 08:47

---
+++ lookup/wine.gd
@@ -0,0 +1,11 @@
+# Remove a program by the Terminal
+$ wine uninstaller
+
+# Remove entries
+$ cd /home/user_name/.local/share/desktop-directories/
+$ ls
+$ rm wine-Programas-WinRAR.directory
+
+
+$ cd /home/luciano/.local/share/applications/wine/Programas
+$ rm -r Winrar
